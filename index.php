4b9a455da6f300e0b5203b24bc9f941f74bbe90c
CarproE3T <Carproe3t@hotmail.com>
2024-07-01 10:00:57 -0500

    index.php

--- index.php
+++ index.php
@@ -4,33 +4,50 @@
     <title>Cartera de Trabajos de Grado</title>
     <style>
         body {
-            
             font-family: Arial, sans-serif;
             text-align: center;
             padding-top: 100px;
+            background: url('https://i.ibb.co/wWXSGTL/Dise-o-sin-t-tulo-1-min.png') no-repeat center center fixed; 
+            background-size: cover;
+            color: white;
         }
 
         .boton {
             background-color: green;
             color: white;
-            padding: 45px 55px;
-            margin: 80px 0;
+            padding: 20px 30px;
+            margin: 20px 0;
             border: none;
             cursor: pointer;
-            width: 800px; /* Ancho fijo para los botones */
-            display: block; /* Asegura que cada botón esté en su propia línea */
+            width: 60%;
+            max-width: 400px; /* Ancho máximo para los botones */
+            display: block;
             margin-left: auto;
             margin-right: auto;
+            font-size: 1.2em;
+            border-radius: 10px; /* Bordes redondeados */
+            box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.3); /* Sombra */
+            transition: background-color 0.3s ease, transform 0.3s ease; /* Transición */
+        }
+
+        .boton:hover {
+            background-color: darkgreen;
+            transform: translateY(-5px); /* Efecto de elevar el botón al pasar el ratón */
         }
 
         h1 {
-            color: black;
+            color: white;
+            text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.6); /* Sombra en el texto */
+        }
+
+        form {
+            margin-bottom: 20px; /* Espaciado entre los formularios */
         }
     </style>
 </head>
 <body>
 
-<h1>Ingenieria Eléctrica</h1>
+<h1> Cartera de Proyectos-Ingenierias Eléctrica y Electrónica</h1>
 
 <form action="estudiante.php" method="post">
     <input type="submit" value="Estudiante" class="boton">
